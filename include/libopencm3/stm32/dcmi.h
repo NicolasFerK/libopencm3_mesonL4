803f3be42b203d2cbfc86668321005bc9c1fd1f0
Nicolas FK <nicolasfernandesk@gmail.com>
2025-04-14 15:58:42 -0300

    Mudada o include de ADC para o L4

--- include/libopencm3/stm32/dcmi.h
+++ include/libopencm3/stm32/dcmi.h
@@ -20,10 +20,12 @@
 #include <libopencm3/cm3/common.h>
 #include <libopencm3/stm32/memorymap.h>
 
-#if defined(STM32F4)
+#if defined(STM32F4) 
 #       include <libopencm3/stm32/f4/dcmi.h>
 #elif defined(STM32F7)
 #       include <libopencm3/stm32/f7/dcmi.h>
+#elif defined(STM32L4)
+#       include <libopencm3/stm32/l4/dcmi.h>
 #else
 #       error "stm32 family not defined."
 #endif
